15dd7fdf96fb31f73c528b5c2955d965012ddfee
ownerr <musimentalois@gmail.com>
2024-06-13 14:09:32 -0700

    third commit

--- frontend/Router/AppRouter.js
+++ frontend/Router/AppRouter.js
@@ -2,17 +2,17 @@
 
 import React from 'react';
 import {Stack} from "expo-router"
-import { View, Text, Button, StyleSheet } from 'react-native';
-import GenerateToken from '../generateToken';
-import ValidateToken from '../validateToken';
-import ViewTokens from '../ViewToken';
+import { StyleSheet } from 'react-native';
+import GenerateToken from '../components/generateToken';
+import ValidateToken from '../components/validateToken';
+import ViewTokens from '../components/ViewToken';
 
 const AppRouter = () => {
   return (
     <Stack>
-      <Stack.Screen name="Generate Token" component={GenerateToken} />
-        <Stack.Screen name="Validate Token" component={ValidateToken} />
-        <Stack.Screen name="View Tokens" component={ViewTokens} />
+      <Stack.Screen name="generateToken" component={GenerateToken} />
+        <Stack.Screen name="validateToken" component={ValidateToken} />
+        <Stack.Screen name="ViewToken" component={ViewTokens} />
     </Stack>
   )
 };
